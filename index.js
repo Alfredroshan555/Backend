07a1f4767d09c95edd42606ece4b183cc1034a61
Alfredroshan555 <alfredroshan555@gmail.com>
2023-10-23 00:26:28 +0530

    Added and updated ReadME File

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { getAllProducts } = require("./products");
 
 const port = 3000;
 
+// Express App
 const app = express();
 
 // Getting value/data from a custom module/function in node.js
@@ -23,24 +24,3 @@ app.get("/products", (req, res) => {
 });
 
 app.listen(port, () => console.log("App is running on port " + " " + port));
-
-// const server = http.createServer((req, res) => {
-//   console.log("A change was made" + port);
-//   res.write(string + " " + port);
-//   res.end();
-// });
-
-// server.listen(port);
-
-// TODO) ==>
-/**
- * Add Express/Nest ==> any backend framework for Node js ===> Maybe try both   ==> DONE
- * Add Nodemon for live server compilation ==> DONE
- * Learn to Add Middlewares ==> Use multiple middlewares and learn how they work in the whole application ==>
- * Create first API using dummy data ==> DONE
- *
- * Add a middleware for some functionalities
- * Fetch data from a local database ==> maybe MongoDb or MySql or some other database
- * Configure Router for the app and seggregate the routes into different router files
- * Add Controllers for different types of functionalities of the application
- */
