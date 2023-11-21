5bf77096286c84e4882ae076035bcf32b0b4ebb7
Alfred <alfredroshan555@gmail.com>
2023-11-21 11:42:32 +0530

    Added mongoDB database to the project

---
+++ config.js
@@ -0,0 +1,10 @@
+// Port Number
+const PORT = 3000;
+
+// MongoDB connection string
+const mongoDBURL =
+  "mongodb+srv://alfredroshan555:willshome@wills.n3hvgr9.mongodb.net/wills?retryWrites=true&w=majority";
+
+module.exports = { mongoDBURL, PORT };
+
+// mongodb+srv://alfredroshan555:willshome@wills.n3hvgr9.mongodb.net/?retryWrites=true&w=majority
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = require("./routes/router");
 const cors = require("cors");
-
-const PORT = 3000;
+const mongoose = require("mongoose");
+const { mongoDBURL, PORT } = require("./config");
 
 // Express App
 const app = express();
@@ -16,5 +16,16 @@ app.use(cors({ origin: "http://localhost:3001", optionsSuccessStatus: 200 }));
 // Middleware to use express Router
 app.use("/", router);
 
-// Run the server Here ==> localhost:portnumber
-app.listen(PORT, () => console.log("Server is running on port: " + "" + PORT));
+// MongoDb connection
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to mongoDb");
+    // Run the server Here ==> localhost:portnumber
+    app.listen(PORT, () =>
+      console.log("Server is running on port: " + "" + PORT)
+    );
+  })
+  .catch((err) => {
+    console.log("Database connection error");
+  });
